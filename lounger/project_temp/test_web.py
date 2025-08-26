8d89fbc42c107d848e3a2fd1b7730d1cc4145869
defnngj <defnngj@gmail.com>
2025-08-27 00:00:23 +0800

    feat: update template

--- lounger/project_temp/test_web.py
+++ lounger/project_temp/test_web.py
@@ -19,11 +19,13 @@ def test_has_title(page: Page, base_url):
 
 def test_get_started_link(page: Page, base_url):
     page.goto(base_url)
+    page.wait_for_load_state("load")
+    page.wait_for_timeout(2000)
     input = page.locator("#sb_form_q")
     input.type("playwright")
     input.press("Enter")
     time.sleep(5)
 
-    # Expects page to have a heading with the name of "Installation | Playwright".
-    search_result = page.get_by_role("link", name='Installation | Playwright').first
+    # Expects page to have a heading with the name of "Fast and reliable end-to-end testing for modern".
+    search_result = page.get_by_role("link", name='Fast and reliable end-to-end testing for modern').first
     expect(search_result).to_be_visible()
