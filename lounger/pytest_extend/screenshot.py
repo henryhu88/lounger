27243565f7094177cff2083e48c95dbba9d598ee
defnngj <defnngj@gmail.com>
2026-04-12 21:29:19 +0800

    :bug: lazy import playwright

--- lounger/pytest_extend/screenshot.py
+++ lounger/pytest_extend/screenshot.py
@@ -1,14 +1,17 @@
 import base64
+from typing import TYPE_CHECKING
 
-from playwright.sync_api import Page
+if TYPE_CHECKING:
+    from playwright.sync_api import Page
 
 
-def screenshot_base64(page: Page):
+def screenshot_base64(page: "Page"):
     """
     Obtain a base64 encoded screenshot
     :param page:
     :return:
     """
+    # Lazy import to avoid import errors when playwright is not properly installed
     img = page.screenshot(type='png', path=None)
     img_base64 = base64.b64encode(img).decode('utf-8')
     return img_base64
